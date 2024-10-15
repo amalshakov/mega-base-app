91f9fac30accb1eff76df83a6e0382a804c1d7c3
Alexander Malshakov <89681115653@mail.ru>
2024-10-15 13:51:34 +0300

    Add main.py

---
+++ fastapi-application/main.py
@@ -0,0 +1,27 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+import uvicorn
+
+from core.config import settings
+from core.models import db_helper
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    await db_helper.create_tables()
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:main_app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
