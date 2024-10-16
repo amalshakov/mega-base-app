7b90abeffa63d0a956b0dbb610d70ad67316579f
Alexander Malshakov <89681115653@mail.ru>
2024-10-16 16:24:12 +0300

    Add USERS_FOR_DB

--- app/main2.py
+++ app/main2.py
@@ -3,15 +3,51 @@ import asyncio
 from utils import crud
 
 
+USERS_FOR_DB = [
+    {
+        "username": "jessika",
+        "email": "jessika@example.fake",
+        "number_upload_files": 27,
+        "name_top_file": "encyclopedia.txt"
+    },
+    {
+        "username": "john",
+        "email": "john@example.fake",
+        "number_upload_files": 805,
+        "name_top_file": "encyclopedia.txt"
+    },
+    {
+        "username": "alexander",
+        "email": "alexander@example.fake",
+        "number_upload_files": 2029,
+        "name_top_file": "cats.jpeg"
+    },
+    {
+        "username": "ben",
+        "email": "ben@example.fake",
+        "number_upload_files": 72,
+        "name_top_file": "cats.jpeg"
+    },
+    {
+        "username": "julie",
+        "email": "julie@example.fake",
+        "number_upload_files": 7828,
+        "name_top_file": "all stars.jpeg"
+    },
+]
+
+
 async def main():
     # await crud.insert_user("jacob", "jacob@example.fake", 33, "dogs.pdf")
+    await crud.insert_users(USERS_FOR_DB)
+
     # await crud.update_user(user_id=7, number_upload_files=34, name_top_file="it.doc")
 
     # users = await crud.get_info_all_users()
     # print(users)
 
-    users = await crud.get_info_all_users_by_top_file("dogs.pdf")
-    print(users)
+    # users = await crud.get_info_all_users_by_top_file("dogs.pdf")
+    # print(users)
 
 
 if __name__ == "__main__":
