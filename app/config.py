5b9676f9c62ce107a274dc7f8042902bbf9341eb
Alexander Malshakov <89681115653@mail.ru>
2024-10-20 11:20:41 +0300

    Add extra for settings

--- app/config.py
+++ app/config.py
@@ -12,16 +12,13 @@ class DatabaseConfig(BaseModel):
     echo: bool = True
     echo_pool: bool = True
 
-    class Config:
-        env_file = ".env"
-
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         case_sensitive=False,
         env_nested_delimiter="__",
-        env_prefix="APP_CONFIG__",
+        extra="allow"
     )
     run: RunConfig = RunConfig()
     db: DatabaseConfig
