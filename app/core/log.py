f3f19f2bf5dd6742cf94a1a47e31845e85880594
Alexander Malshakov <89681115653@mail.ru>
2024-10-22 16:29:52 +0300

    Change logger on write to file

--- app/core/log.py
+++ app/core/log.py
@@ -1,15 +1,26 @@
 import logging
+import os
 
 
-def get_logger(log_level: str = 'INFO') -> logging.Logger:
+def get_logger(
+        log_level: str = 'INFO',
+        log_file: str = "app.log"
+) -> logging.Logger:
+    log_dir = "logs"
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    log_file_path = os.path.join(log_dir, log_file)
+
     logger = logging.getLogger(__name__)
     logger.setLevel(log_level.upper())
     logger.handlers.clear()
 
-    console_handler = logging.StreamHandler()
-    logger.addHandler(console_handler)
+    file_handler = logging.FileHandler(log_file_path, encoding="utf-8")
 
     formatter = logging.Formatter('%(asctime)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
 
     return logger
