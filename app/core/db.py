cf727846f2264f32e977efa960cb3c03c7d3666a
Alexander Malshakov <89681115653@mail.ru>
2024-10-22 16:30:23 +0300

    Add global try except

--- app/core/db.py
+++ app/core/db.py
@@ -30,39 +30,39 @@ class DB:
         await self.engine.dispose()
 
     async def create_tables(self):
-        query = text(
-            "CREATE TABLE IF NOT EXISTS users "
-            "("
-            "id SERIAL PRIMARY KEY, "
-            "username VARCHAR(50) NOT NULL, "
-            "email VARCHAR(100) NOT NULL UNIQUE, "
-            "number_upload_files INT, "
-            "name_top_file VARCHAR(255)"
-            ")"
-            ";"
-        )
-        async with self.engine.begin() as conn:
-            try:
+        try:
+            query = text(
+                "CREATE TABLE IF NOT EXISTS users "
+                "("
+                "id SERIAL PRIMARY KEY, "
+                "username VARCHAR(50) NOT NULL, "
+                "email VARCHAR(100) NOT NULL UNIQUE, "
+                "number_upload_files INT, "
+                "name_top_file VARCHAR(255)"
+                ")"
+                ";"
+            )
+            async with self.engine.begin() as conn:
                 await conn.execute(query)
                 logger.info("Таблица 'users' успешно создана или уже существует.")
-            except SQLAlchemyError as error:
-                logger.error(f"Ошибка при создании таблицы: '{error}'")
-                raise
+        except Exception as error:
+            logger.error(f"Ошибка при создании таблицы: '{error}'")
+            raise
 
     async def insert_user(
-            self,
-            username: str,
-            email: EmailStr,
-            number_upload_files: int = 0,
-            name_top_file: str = None,
+        self,
+        username: str,
+        email: EmailStr,
+        number_upload_files: int = 0,
+        name_top_file: str = None,
     ):
-        query = text(
-            "INSERT INTO users (username, email, number_upload_files, name_top_file) "
-            "VALUES (:username, :email, :number_upload_files, :name_top_file)"
-            ";"
-        )
-        async with self.engine.begin() as conn:
-            try:
+        try:
+            query = text(
+                "INSERT INTO users (username, email, number_upload_files, name_top_file) "
+                "VALUES (:username, :email, :number_upload_files, :name_top_file)"
+                ";"
+            )
+            async with self.engine.begin() as conn:
                 await conn.execute(
                     query,
                     {
@@ -73,22 +73,22 @@ class DB:
                     },
                 )
                 logger.info(f"Пользователь '{username}' с email '{email}' успешно добавлен.")
-            except SQLAlchemyError as error:
-                logger.info(f"Ошибка при добавлении пользователя '{username}' с email '{email}': '{error}'")
-                raise
+        except Exception as error:
+            logger.info(f"Ошибка при добавлении пользователя '{username}' с email '{email}': '{error}'")
+            raise
 
     async def insert_users(
-            self,
-            users: list[dict[str, int]],
+        self,
+        users: list[dict[str, int]],
     ):
-        query = text(
-            "INSERT INTO users (username, email, number_upload_files, name_top_file) "
-            "VALUES (:username, :email, :number_upload_files, :name_top_file)"
-            ";"
-        )
-        async with self.engine.begin() as conn:
-            for user in users:
-                try:
+        try:
+            query = text(
+                "INSERT INTO users (username, email, number_upload_files, name_top_file) "
+                "VALUES (:username, :email, :number_upload_files, :name_top_file)"
+                ";"
+            )
+            async with self.engine.begin() as conn:
+                for user in users:
                     await conn.execute(
                         query,
                         {
@@ -99,72 +99,68 @@ class DB:
                         },
                     )
                     logger.info(f"Пользователь '{user['username']}' с email '{user['email']}' успешно добавлен.")
-                except SQLAlchemyError as error:
-                    logger.error(f"Ошибка при вставке пользователя '{user['username']}' с email '{user['email']}': {error}")
-                    raise
+        except Exception as error:
+            logger.error(f"Ошибка при добавлении пользователя '{user['username']}' с email '{user['email']}': {error}")
+            raise
 
     async def update_user(
-            self,
-            user_id: int, **kwargs,
+        self,
+        user_id: int, **kwargs,
     ):
-        async with self.engine.begin() as conn:
-            for key, value in kwargs.items():
-                if value is not None:
-                    query = text(
-                        f"UPDATE users "
-                        f"SET {key}='{value}' "
-                        f"WHERE id={user_id}"
-                        f";"
-                    )
-                else:
-                    query = text(
-                        f"UPDATE users "
-                        f"SET {key}=NULL "
-                        f"WHERE id={user_id}"
-                        f";"
-                    )
-                try:
+        try:
+            async with self.engine.begin() as conn:
+                for key, value in kwargs.items():
+                    if value is not None:
+                        query = text(
+                            f"UPDATE users "
+                            f"SET {key}='{value}' "
+                            f"WHERE id={user_id}"
+                            f";"
+                        )
+                    else:
+                        query = text(
+                            f"UPDATE users "
+                            f"SET {key}=NULL "
+                            f"WHERE id={user_id}"
+                            f";"
+                        )
                     await conn.execute(query)
                     logger.info(f"Пользователь с id '{user_id}' -> успешно обновлено поле '{key}' на значение '{value}'")
-                except SQLAlchemyError as error:
-                    logger.error(f"Пользователь с id '{user_id}' -> ошибка обновления поля '{key}' на значение '{value}'. Ошибка: '{error}'")
-                    raise
+        except Exception as error:
+            logger.error(f"Пользователь с id '{user_id}' -> ошибка обновления поля '{key}' на значение '{value}'. Ошибка: '{error}'")
+            raise
 
     async def get_info_all_users(self):
-        query = text(
-            "SELECT id, username, email, number_upload_files, name_top_file "
-            "FROM users"
-            ";"
-        )
-        async with self.engine.begin() as conn:
-            try:
+        try:
+            query = text("SELECT * FROM users;")
+            async with self.engine.begin() as conn:
                 result = await conn.execute(query)
                 logger.info("Запрос к БД на получение всех пользователей выполнен успешно")
-            except SQLAlchemyError as error:
-                logger.error(f"Ошибка при запросе к БД на получение всех пользователей. Ошибка: '{error}'")
-                raise
             users = result.mappings().fetchall()
             return users
+        except Exception as error:
+            logger.error(f"Ошибка при запросе к БД на получение всех пользователей. Ошибка: '{error}'")
+            raise
 
     async def get_info_all_users_by_top_file(
-            self,
-            name_top_file,
+        self,
+        name_top_file,
     ):
-        query = text(
-            f"SELECT id, username, email, number_upload_files, name_top_file "
-            f"FROM users "
-            f"WHERE name_top_file = '{name_top_file}'"
-            f";"
-        )
-        async with self.engine.begin() as conn:
-            try:
+        try:
+            query = text(
+                f"SELECT * "
+                f"FROM users "
+                f"WHERE name_top_file = '{name_top_file}'"
+                f";"
+            )
+            async with self.engine.begin() as conn:
                 result = await conn.execute(query, {"name_top_file": name_top_file})
                 logger.info(f"Запрос к БД на получение пользователей с полем 'name_top_file': '{name_top_file}' выполнен успешно")
-            except SQLAlchemyError as error:
-                logger.error(f"Ошибка при запросе к БД на получение всех пользователей с полем 'name_top_file': '{name_top_file}'. Ошибка: '{error}'")
-                raise
             users = result.mappings().fetchall()
             return users
+        except Exception as error:
+            logger.error(f"Ошибка при запросе к БД на получение всех пользователей с полем 'name_top_file': '{name_top_file}'. Ошибка: '{error}'")
+            raise
 
 
 db = DB(
