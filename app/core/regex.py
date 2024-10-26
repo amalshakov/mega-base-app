9ab30bc43e271da12ee421a31154f615448dd0cf
Alexander Malshakov <89681115653@mail.ru>
2024-10-26 11:30:42 +0300

    Add logging and refactor

--- app/core/regex.py
+++ app/core/regex.py
@@ -1,8 +1,15 @@
 import re
 
+from core.log import get_logger
+
+logger = get_logger()
+
 valid_email = [
     "zina.sidorova@megafon.ru",
-    "denis.dyatlov@megafon.ru"
+    "denis.dyatlov@megafon.ru",
+    "denis.petrov@megafon.ru",
+    "oleg.ivanov@megafon.ru",
+    "oleg.pe.ivanov@megafon.ru",
 ]
 
 invalid_email = [
@@ -17,19 +24,27 @@ def validate_email(
         surname: str,
         name: str,
         patronymic: str,
-) -> str:
+):
     pattern = r"^[a-z]+\.[a-z]+@megafon\.ru$"
 
     if re.match(pattern, email):
         if email in valid_email:
-            email = f"{name}.{patronymic[:2]}.{surname}@megafon.ru"
-            if email in valid_email:
-                print(f"Ваш email '{email}' УЖЕ занят. Обратитесь к администратору!")
-                return email
+            new_email = f"{name}.{patronymic[:2]}.{surname}@megafon.ru"
+            if new_email in valid_email:
+                print(f"Ваши email'ы '{email}' и '{new_email}' УЖЕ заняты! Обратитесь к администратору!")
+                logger.info(f"Не получилось сформировать email для ФИО: '{surname} {name} {patronymic}'. Данные email'ы уже заняты: '{email}' и '{new_email}'")
+                return False
+            valid_email.append(new_email)
+            print(f"Данный email '{email}' уже занят!")
+            print(f"Ваш email будет такой '{new_email}', валиден. Добавлен в массив valid_email.")
+            logger.info(f"Сформирован email '{new_email}' для ФИО: '{surname} {name} {patronymic}'. Email '{email}' занят!")
+            return new_email
         valid_email.append(email)
         print(f"Ваш email '{email}' валиден. Добавлен в массив valid_email.")
+        logger.info(f"Сформирован email '{email}' для ФИО: '{surname} {name} {patronymic}'.")
         return email
     else:
         invalid_email.append(email)
         print(f"Ваш email '{email}' не валиден! Добавлен в массив invalid_email.")
-        return email
+        logger.info(f"Сформированый email '{email}' для ФИО: '{surname} {name} {patronymic}' не валиден.")
+        return False
