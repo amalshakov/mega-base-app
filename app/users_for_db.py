a8c9b124bc27d9b8655b0963f20a134f466b7b3c
Alexander Malshakov <89681115653@mail.ru>
2024-10-26 11:28:16 +0300

    Relocate users_for_db

---
+++ app/users_for_db.py
@@ -0,0 +1,32 @@
+USERS_FOR_DB = [
+    {
+        "username": "jessika",
+        "email": None,
+        "number_upload_files": 27,
+        "name_top_file": "encyclopedia.txt"
+    },
+    {
+        "username": "john",
+        "email": None,
+        "number_upload_files": 805,
+        "name_top_file": "encyclopedia.txt"
+    },
+    {
+        "username": "alexander",
+        "email": None,
+        "number_upload_files": 2029,
+        "name_top_file": "cats.jpeg"
+    },
+    {
+        "username": "ben",
+        "email": None,
+        "number_upload_files": 72,
+        "name_top_file": "cats.jpeg"
+    },
+    {
+        "username": "julie",
+        "email": None,
+        "number_upload_files": 7828,
+        "name_top_file": "all stars.jpeg"
+    },
+]
